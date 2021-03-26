75843759c9f41a6aa1c929bd1d78fe3f1364af22
KeylaZaruma <kzaruma1@utmachala.edu.ec>
2021-03-26 18:35:04 -0500

    make it better

--- routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const api = express.Router();
-const controller = require('./controller');
-
-api.get('/users', controller.getAll);
-api.get('/user/:id', controller.getById);
-
-module.exports = api;
